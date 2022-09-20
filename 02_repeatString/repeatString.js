252ce147ba423c0c17fae99a778b636dc18422ef
clearfeather <info@clearfeather.com>
2022-09-20 16:30:22 -0500

    01 and 02 exercies done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) return 'ERROR';
+    else return string.repeat(times);
 };
 
 // Do not edit below this line
