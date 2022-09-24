fa7a8ef2552f8d801614003f3944459652def011
clearfeather <info@clearfeather.com>
2022-09-23 20:10:34 -0500

    add new exercies completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
+    // loop through array
+    array.forEach((item) => {
+        // if cut value exists
+        if (!args.includes(item)) {
+            // remove cut value
+            newArray.push(item);
+        }
+    });
+    // return array
+    return newArray;
 };
 
 // Do not edit below this line
