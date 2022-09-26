a1688af8403f87b6442a0504298d6f0b09f24884
clearfeather <info@clearfeather.com>
2022-09-26 18:30:07 -0500

    add new completed exerc

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+// fahrenheit to celsius
+const ftoc = function(f) {
+  // [°C] = ([°F] − 32) × 5⁄9
+  c = ((f - 32) * 5/9);
+  return Math.round(c * 10) / 10;
 };
 
-const ctof = function() {
-
+// celsius to fahrenheit
+const ctof = function(c) {
+  // [°F] = [°C] × 9⁄5 + 32
+  f = ((c * 9/5) + 32);
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
