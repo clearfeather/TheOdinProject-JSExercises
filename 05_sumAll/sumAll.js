88693a9266e67dc7f420ba1984695221edecebaa
clearfeather <info@clearfeather.com>
2022-09-26 09:58:33 -0500

    add sum exc

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let total = 0;
+    if (Number.isInteger(num1) && num1 > 0 && Number.isInteger(num2) && num2 > 0 && num1 < num2) {
+        for (let i = num1; i < num2 + 1; i++) {
+            total += i;
+        } return total;
 
+    } else if (num1 > num2) {
+        //do it backwards
+        for (let i = num2; i < num1 + 1; i++) {
+            total += i;
+        } return total;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
