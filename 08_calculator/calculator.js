0729bf08bc7e03a9556b86c416b9cadb4b965cef
clearfeather <info@clearfeather.com>
2022-09-28 07:41:30 -0500

    add calc exc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,24 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	for (let num of array) {
+    total += num;
+  } return total;
+  //or use: return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 0;
+  for (let num = 0; num < array.length; num++) {
+    total *= array[num];
+  } return total;
 };
 
 const power = function() {
